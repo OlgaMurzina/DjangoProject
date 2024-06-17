df4e6f23f9105d126e7b4a0ac685050b20522652
OlgaMurzina <74879104+OlgaMurzina@users.noreply.github.com>
2024-06-17 19:06:05 +0300

    add files

--- quizz/first_quizz/views.py
+++ quizz/first_quizz/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    themes = ['A', 'B', 'C', 'D', 'E', 'F']
+    context = {'themes': [(i + 1, themes[i]) for i in range(len(themes))]}
+    return render(request, 'first_quizz/index.html', context)
